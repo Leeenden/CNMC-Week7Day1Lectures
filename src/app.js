7e3cd3cbb7b0aa22d01f2d297ed26a0640591fe9
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-04-25 16:28:26 +0100

    printing all movies from a single input

--- src/app.js
+++ src/app.js
@@ -5,13 +5,33 @@ const {Movie, movieArr} =  require("./utils");
 
 const app = (yargsObj) => {
     if (yargsObj.add) {
-        const movie = new Movie(yargsObj.title, yargsObj.actor);
-        movie.add();
-        console.log(movieArr);
-    } else  {
+        for(let i = 0; i < yargsObj.numOfMovies; i ++){
+            const movie = new Movie(yargsObj.title[i], yargsObj.actor[i], yargsObj.genre[i], yargsObj.rating[i]);
+            movie.add();
+        }
+        console.log(movieArr)
+    } else {
         console.log("Incorrect Command");
     }
 };
 
 
 app(yargs.argv);
+
+// const app = (param) => {
+//     if (param.add) {
+//         for(let i = 0; i < param.numberOfMovies; i++) {
+//             const movie = new Movie(
+//                 param.movie[i],
+//                 param.actor[i],
+//                 param.year[i],
+//                 param.rating[i]
+//             )
+//             movie.add()
+//         }
+        
+//         console.log(movieArr)
+//     }else {
+//         console.log("Incorrect Command")
+//     }
+// }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,11 @@
 const movieArr = [];
 
 class Movie {
-    constructor(title, actor = "Not specified"){
+    constructor(title, actor, genre, rating = "Not specified"){
         this.title = title;
         this.actor = actor;
+        this.genre = genre;
+        this.rating = rating;
     }
     add(){
         movieArr.push(this)
